da469260a9b85b83650f51e71f23c853b839b433
X <qiaorui.xiang@gmail.com>
2017-12-05 15:06:45 +0100

    linux script done

--- project/run.sh
+++ project/run.sh
@@ -4,7 +4,7 @@
 echo "->Start Scripting"
 X_ARGS='ilp/main.mod ilp/config.dat'
 
-if [ $1 == "test" ]; then
+if [ $1 = "test" ]; then
   X_ARGS='ilp/unit_test.mod ilp/config.dat'
 fi
 if [ $# -eq 2 ]; then
@@ -26,8 +26,18 @@ case "$(uname -s)" in
     echo '  Linux Detected, Running...'
     P="$(printenv LD_LIBRARY_PATH)"
     echo $P
-    if [ "$P" == "" ]; then 
+    if [ "$P" = "" ]; then 
        echo "Create LD_LIBRARY_PATH"
+       LD_LIBRARY_PATH="$(find /opt/ -path "*/CPLEX_*/opl/bin/*" -type d -iname 'x*')"
+       if [ "$LD_LIBRARY_PATH" = "" ]; then
+           echo "No oplrun direcoty found, exit on error"
+           exit
+       else
+           echo "$P"
+           export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+           export PATH=:$LD_LIBRARY_PATH
+           oplrun $X_ARGS
+       fi
     fi
      # find Applications/ -path "*/CPLEX_*/*" -type d -iname 'cplex'
      #LD_LIBRARY_PATH=/opt/ibm/ILOG/CPLEX_Studio1251/opl/bin/x86-64_sles10_4.1
